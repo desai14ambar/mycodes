e8ce26588015f4fc3fbd9c0eaf9940eb1f214cb0
desai14ambar <ambar@umich.edu>
2016-05-06 23:59:01 -0400

    Added files via upload
    IMU DUE working code with gravity component removed from acc data. 5/6/2016

---
+++ getBodyAcc.h
@@ -0,0 +1,55 @@
+#ifndef getBodyAcc_h
+#define getBodyAcc_h
+
+class getBodyAcc
+{
+	public:
+		getBodyAcc(float ax, float ay, float az, float ag1, float ag2, float ag3, float fac){
+		accall[0] = ax;
+		accall[1] = ay;
+		accall[2] = az;
+
+                alpha = ag1;
+                beta  = ag2;
+                gama  = ag3;
+                
+		factr = fac;
+		};
+		
+		void readAcc(float * bodyacc);
+		
+		private:
+			float accall[3];
+			float factr;
+                        float gbody[3];
+                        float alpha;
+                        float beta;
+                        float gama;
+                        const float g  = 9.8f; // m/s^2
+                                               
+};		
+
+
+// below code to get acc in body frame
+void getBodyAcc::readAcc(float * bodyacc) {
+    
+  alpha = alpha *M_PI/180;
+  beta  = beta  *M_PI/180;
+  gama  = gama  *M_PI/180;
+  
+  gbody[0] = g*( cos(alpha) * sin(beta) * cos(gama) + sin(alpha) * sin(gama) ) ;
+  gbody[1] = g*( sin(alpha) * sin(beta) * cos(gama) - cos(alpha) * sin(gama) ) ;
+  gbody[2] = g*( cos(beta)  * cos(gama) );
+
+  bodyacc[0] = accall[0] /factr - gbody[0];
+  bodyacc[1] = accall[1] /factr - gbody[1];
+  bodyacc[2] = accall[2] /factr - gbody[2];
+
+//  bodyacc[0] = alpha*accall[0];
+//  bodyacc[1] = accall[1];
+//  bodyacc[2] = accall[2];
+//return (bodyacc);
+}
+
+#endif
+
---
+++ newSix.ino
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////////////////
+// Arduino firmware for use with FreeSixCube processing example
+////////////////////////////////////////////////////////////
+
+// The libraries included below have been adapted to work on Arduino DUE by PedalSensor team 
+
+#include <FreeSixIMU_DUE.h>
+#include <FIMU_ADXL345_DUE.h>
+#include <FIMU_ITG3200_DUE.h>
+//#include "kalman_acc.h"
+#include "getBodyAcc.h"
+
+#define DEBUG
+#ifdef DEBUG
+#include "DebugUtils.h"
+#endif
+
+//#include "CommunicationUtils.h"
+#include <Wire.h>
+//#include <math.h>
+
+float accel[4];
+float bacc[3];
+
+float euangles[3];
+float fcalib; //calibration factor
+
+// Set the FreeIMU object
+FreeSixIMU_DUE my3IMU = FreeSixIMU_DUE();
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+
+  delay(5);
+  my3IMU.init();
+  delay(5);
+  
+  fcalib = my3IMU.calibAcc();
+  
+}
+
+void loop() { 
+
+//read acceleration values
+    my3IMU.getValues(accel);
+    my3IMU.getAngles(euangles);
+    //my3IMU.getBodyAcc(bacc);
+    
+  Serial.print("\t Ax:");     Serial.print(accel[0]);
+  Serial.print("\t Ay:");     Serial.print(accel[1]);
+  Serial.print("\t Az:");     Serial.print(accel[2]);
+  Serial.print("\t g:");      Serial.print(accel[2]/fcalib);
+  
+  getBodyAcc accltn(accel[0],accel[1],accel[2],euangles[0], euangles[1], euangles[2], fcalib);
+  accltn.readAcc(&bacc[0]);
+  
+  Serial.print("\t Body Ax:");     Serial.print(bacc[0]);
+  Serial.print("\t Body Ay:");     Serial.print(bacc[1]);
+  Serial.print("\t Body Az:");     Serial.print(bacc[2]);
+  Serial.println(""); //line break
+  delay(100);
+    
+}
+
+
